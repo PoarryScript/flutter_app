6e913d889d4c69ff49c654f81c1b64fbe954ed7d
Po <719151128@qq.com>
2018-09-21 15:02:43 +0800

    ADD:  jump route page

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,9 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup Name Generator', home: RandomWords());
+    return MaterialApp(title: 'Startup Name Generator',
+        theme: new ThemeData(primaryColor: Colors.white),
+        home: RandomWords());
   }
 }
 
