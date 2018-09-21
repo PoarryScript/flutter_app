e90aa79bb79b792129d642f5e3067deb87812ad6
Po <719151128@qq.com>
2018-09-21 18:48:47 +0800

    ADD BaseUI

---
+++ lib/BaseUI.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new Center(
+    child: new Text(
+      'Hello world',
+      textDirection: TextDirection.ltr,
+    ),
+  ));
+}
