54582c31d6ef3306fdeebb2b521c0f177c6b04da
Po <719151128@qq.com>
2018-09-22 14:14:06 +0800

    BaseUI widget

--- lib/ToturialHome.dart
+++ lib/ToturialHome.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    title: 'Flutter Tutorial',
-    home: new TutorialHome(),
+    /* title: 'Flutter Tutorial',
+    home: new TutorialHome(),*/
+    title: 'Shopping App',
+    home: new ShoppingList(
+      products: <Product>[
+        new Product(name: 'Eggs'),
+        new Product(name: 'Flour'),
+        new Product(name: 'Chocolate chips'),
+      ],
+    ),
   ));
 }
 
@@ -27,6 +35,167 @@ class TutorialHome extends StatelessWidget {
       ),
       floatingActionButton: new FloatingActionButton(
           tooltip: 'Add', child: new Icon(Icons.add), onPressed: null),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: () {
+        print('My Button was tapped');
+      },
+      child: new Container(
+        height: 36.0,
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: new BoxDecoration(
+          borderRadius: new BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: new Center(
+          child: new Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => new _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new CounterIncrementor(
+          onPressed: _increment,
+        ),
+        new CounterDisplay(
+          count: _counter,
+        )
+      ],
+    );
+  }
+}
+
+class CounterDisplay extends StatelessWidget {
+  final int count;
+
+  CounterDisplay({this.count});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Text('Count:$count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  CounterIncrementor({this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return new RaisedButton(
+      onPressed: onPressed,
+      child: new Text('Increment'),
+    );
+  }
+}
+
+class Product {
+  const Product({this.name});
+
+  final String name;
+}
+
+typedef void CartChangeCallback(Product product, bool inCart);
+
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangeCallback onCartChanged;
+
+  ShoppingListItem({Product product, this.inCart, this.onCartChanged})
+      : product = product,
+        super(key: new ObjectKey(product));
+
+  Color _getcolor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+    return new TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      onTap: () {
+        onCartChanged(product, !inCart);
+      },
+      leading: new CircleAvatar(
+        backgroundColor: _getcolor(context),
+        child: new Text(product.name[0]),
+      ),
+      title: new Text(product.name, style: _getTextStyle(context)),
+    );
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  final List<Product> products;
+
+  ShoppingList({Key key, this.products}) : super(key: key);
+
+  @override
+  _ShoppingListState createState() => new _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  Set<Product> _shoppingCart = new Set<Product>();
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (inCart)
+        _shoppingCart.add(product);
+      else
+        _shoppingCart.remove(product);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Shopping List'),
+      ),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: widget.products.map((Product product) {
+          return new ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
     );
   }
 }
