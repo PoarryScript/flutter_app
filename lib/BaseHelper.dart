d366697e76f81dd82134820ffb5e5b7efd192045
Po <719151128@qq.com>
2018-10-06 11:34:03 +0800

    ADD: Http

---
+++ lib/BaseHelper.dart
@@ -0,0 +1,16 @@
+import 'src/dio.dart';
+
+main() async {
+  var dio = new Dio();
+  dio.interceptor.response.onError = (DioError error) {
+    print(error);
+    print(error.response.statusCode);
+  };
+  try {
+    Response r = await dio.get("https://www.instagram.com");
+    print(r.headers);
+  } on DioError catch (e) {
+    print(e);
+    print(e.response.statusCode);
+  }
+}
---
+++ lib/src/dio.dart
@@ -0,0 +1,16 @@
+/// A powerful Http client for Dart, which supports Interceptors,
+/// Global configuration, FormData, File downloading etc. and Dio is
+/// very easy to use.
+///
+/// A open source project authorized by [https://flutterchina.club](https://flutterchina.club).
+library dio;
+
+export 'dio/dio.dart';
+export 'dio/form_data.dart';
+export 'dio/dio_error.dart';
+export 'dio/transformer.dart';
+export 'dio/interceptor.dart';
+export 'dio/options.dart';
+export 'dio/response.dart';
+export 'dio/cancel_token.dart';
+export 'dio/upload_file_info.dart';
